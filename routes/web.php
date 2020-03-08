da27a1fd8a78c0c32d8c3385bb4043e781db66ba
murathe <isaacsm07p@gmail.com>
2020-03-08 16:33:04 +0300

    Redefine home route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@
 
 use App\Http\Controllers\contentsController;
 
-Route::get('/','contentsController@index');  //controller styles for urls and routes
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'welcome');
+Route::get('/contents','contentsController@index');  //controller styles for urls and routes
 Route::get('images','contentsController@images');  
 Route::get('videos','contentsController@videos');  
 Route::get('texts','contentsController@texts'); 
