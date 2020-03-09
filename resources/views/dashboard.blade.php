83b2f602de39c17ecbe11dcc85540d96a1010cff
wamakimaN <wamamoses@gmail.com>
2020-03-09 07:07:24 +0300

    add sidebar links

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,89 @@
+
+<div class="sidebar" data-color="orange" data-background-color="white" data-image="">
+  <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+  <div class="logo">
+    <a href="https://creative-tim.com/" class="simple-text logo-normal">
+    
+    </a>
+  </div>
+  <div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">dashboard</i>
+            <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+          <i><img style="width:25px" src=""></i>
+          <p>
+            <b class="caret"></b>
+          </p>
+        </a>
+        <div class="collapse show" id="laravelExample">
+          <ul class="nav">
+            <li class="">
+              <a class="nav-link" href="">
+                <span class="sidebar-mini"> UP </span>
+                <span class="sidebar-normal"> </span>
+              </a>
+            </li>
+            <li class="">
+              <a class="nav-link" href="">
+                <span class="sidebar-mini"> UM </span>
+                <span class="sidebar-normal"> </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">content_paste</i>
+            <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">library_books</i>
+            <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">bubble_chart</i>
+          <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">location_ons</i>
+            <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">notifications</i>
+          <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">language</i>
+          <p></p>
+        </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="">
+          <i class="material-icons">unarchive</i>
+          <p></p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
