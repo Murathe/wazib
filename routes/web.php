1859beec9575b83a097dee18a1db7285f4d4b6b9
murathe <isaacsm07p@gmail.com>
2020-03-10 11:01:24 +0300

    Resolve conflicts(web.php)

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('home');
-});
-Auth::routes();
-Route::get('/addTherapist', function(){
-    return view('save_therapists');
-});
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/login', 'Auth\LoginController@authenticate'); 
-Route::get('/addTherapist', function(){
-    return view('save_therapists');
-}); 
-Route::post('/addNewTherapistToDB', 'HomeController@addNewTherapistToDB'); 
-Route::get('/displayTherapists', 'HomeController@displayAllTherapists');
-Route::get('/createAssessment', 'HomeController@createAssessment');
-Route::get('/logout', 'HomeController@logout'); 
-=======
 
 use App\Http\Controllers\contentsController;
 
@@ -50,7 +32,6 @@ Route::post('audios','contentsController@audios');
 
 
 
->>>>>>> 890a18a86a29e944db45f84960d553d4bbc71cac
     Route::get('laraquiz/home', 'HomeController@laraquizIndex');
     Route::resource('tests', 'TestsController');
     Route::resource('roles', 'RolesController');
@@ -66,30 +47,17 @@ Route::post('audios','contentsController@audios');
     Route::post('questions_options_mass_destroy', ['uses' => 'QuestionsOptionsController@massDestroy', 'as' => 'questions_options.mass_destroy']);
     Route::resource('results', 'ResultsController');
     Route::post('results_mass_destroy', ['uses' => 'ResultsController@massDestroy', 'as' => 'results.mass_destroy']);
-<<<<<<< HEAD
-
-use App\Http\Controllers\contentsController;
-
-Route::get('contents','contentsController@index');  //controller styles for urls and routes
-Route::get('images','contentsController@images');  
-Route::get('videos','contentsController@videos');  
-Route::get('texts','contentsController@texts'); 
-Route::get('audios','contentsController@audios'); 
-=======
 
 // Route::get('/','contentsController@index');  //controller styles for urls and routes
 Route::get('images','contentsController@images');  
 Route::get('videos','contentsController@videos');  
 Route::get('texts','contentsController@texts'); 
 
->>>>>>> 890a18a86a29e944db45f84960d553d4bbc71cac
 Route::post('images','contentsController@images');
 Route::post('videos','contentsController@videos');
 Route::post('texts','contentsController@texts');
 Route::post('audios','contentsController@audios');
 
-<<<<<<< HEAD
-=======
 
 
 
@@ -97,7 +65,6 @@ Route::post('audios','contentsController@audios');
 //     return view('contents');             //closure style for urls
 // });
 
->>>>>>> 890a18a86a29e944db45f84960d553d4bbc71cac
 Route::get('/', function () {
     return view('welcome');
 });
