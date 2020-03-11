5d7138f52836518ccb6e4a8fcb33a8c4eebd249d
murathe <isaacsm07p@gmail.com>
2020-03-11 13:05:41 +0300

    Correct columns spanning

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,40 +1,26 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col-md-1">
+    <div class="col-sm-2 col-xs-1 col-md-1">
     </div>
-    <div class="col-md-5">
+    <div class="col-sm-3 col-md-5 col-xs-1">
       <div class="landingc card">
-
-        <!-- Card image -->
-        <img class="card-img-top" src="img/manager.svg" alt="Card image cap">
-
-        <!-- Card content -->
+        <img class="card-img-top img-responsive" src="img/manager.svg" alt="Card image cap">
         <div class="card-body">
-          <!-- Button -->
           <a href="{{ route('login') }}"><button class="btn btn-outline-info">Wazi Admin</button></a>
         </div>
       </div>
     </div>
-    <div class="col-md-5">
+    <div class="col-sm-3 col-md-5 col-xs-1">
       <div class="landingc card">
-
-        <!-- Card image -->
         <img class="card-img-top" src="img/consult.svg" alt="Card image cap">
-
-        <!-- Card content -->
         <div class="card-body">
-          <!-- Button -->
-          <button class="btn btn-danger">Service Provider</button>
+          <button class="btn btn-outline-info">Service Provider</button>
         </div>
-
       </div>
     </div>
-
-    <div class="col-md-1">
-
+    <div class="col-sm-2 col-xs-1 col-md-1">
     </div>
   </div>
   @endsection
