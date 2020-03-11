c199288cd13cda362cd3f78f403755fedcdbe2ce
murathe <isaacsm07p@gmail.com>
2020-03-11 13:21:04 +0300

    Corrected landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container mt-10">
   <div class="row">
     <div class="col-sm-2 col-xs-1 col-md-1">
     </div>
-    <div class="col-sm-3 col-md-5 col-xs-1">
-      <div class="landingc card">
+    <div class="col-sm-3 col-md-5 col-xs-12">
+      <div class="landing card">
         <img class="card-img-top img-responsive" src="img/manager.svg" alt="Card image cap">
-        <div class="card-body">
+        <div class="card-body text-center">
           <a href="{{ route('login') }}"><button class="btn btn-outline-info">Wazi Admin</button></a>
         </div>
       </div>
     </div>
-    <div class="col-sm-3 col-md-5 col-xs-1">
+    <div class="col-sm-3 col-md-5 col-xs-12">
       <div class="landingc card">
-        <img class="card-img-top" src="img/consult.svg" alt="Card image cap">
+        <img class="card-img-top img-responsive" src="img/consult.svg" alt="Card image cap">
         <div class="card-body">
           <button class="btn btn-outline-info">Service Provider</button>
         </div>
